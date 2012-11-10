2e17751c1c607abc78f7bd5f895416054520e012
stormmerc <melanie.mariotti@gmail.com>
2012-11-10 12:06:25 -0800

    Update javascripts/player1.js

--- javascripts/player1.js
+++ javascripts/player1.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
 //@param action : string : the webservice method to send a request to
 //@param data : object : an object to be serialized an sent as a request to the webservice
 function sendAJAX() {
-	$.get("http://api.espn.com/v1/sports/basketball/womens-college-basketball/athletes/5934?apikey=yeary2uryb7w7vru4z7j5cu3",
+	$.get("http://api.espn.com/v1/sports/basketball/womens-college-basketball/athletes/14033?apikey=yeary2uryb7w7vru4z7j5cu3",
 		function(data){
 			$('#PlayerName').text(data.sports[0].leagues[0].athletes[0].fullName);
 		}, "json");
