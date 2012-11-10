ab00f3c3b4862e1cb21d6c15bfc375c3e03779e0
Peter Ma <peterma@dn0a160b81.sunet>
2012-11-10 11:48:55 -0800

    put name in

--- javascripts/player1.js
+++ javascripts/player1.js
@@ -9,17 +9,7 @@ $(document).ready(function () {
 //@param data : object : an object to be serialized an sent as a request to the webservice
 function sendAJAX() {
 	$.get("http://api.espn.com/v1/sports/basketball/womens-college-basketball/athletes/5934?apikey=yeary2uryb7w7vru4z7j5cu3",
-	   function(data){
-			dispatchAJAXResponse(data);
-	   }, "json");
-}
-
-//dispatchAJAXResponse
-//forward the webservice response to the appropriate handler method
-//
-//@param action : string : the webservice method the request was sent to
-//@param request : object : the object sent in the request
-//@param response : object : the object received as a response
-function dispatchAJAXResponse(response) {
-
+		function(data){
+			$('#PlayerName').text(data.sports[0].leagues[0].athletes[0].fullName);
+		}, "json");
 }
